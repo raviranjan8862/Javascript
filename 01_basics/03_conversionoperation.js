29ad0d266b54526b5e11b499756988f4f4143fe3
raviranjan8862 <121994910+raviranjan8862@users.noreply.github.com>
2024-01-29 12:41:32 +0000

    conversionoperation

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,43 @@
+let score="33"
+
+console.log(typeof score)
+
+let  valueInNumber=Number(score)
+
+console.log(typeof valueInNumber)
+
+let someNumber=33
+
+let stringNumber=String(someNumber)
+
+console.log(typeof stringNumber)
+
+
+//************************************OPerations ***************************/
+
+let value=3
+
+let negValue=-value
+
+//console.log(negValue)
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2**2)
+//console.log(2%3)
+
+
+/*
+let str1="Ravi"
+let str2="ranjan"
+
+let str3=str1+str2
+
+console.log(str3)
+
+console.log("1"+2)
+console.log(1+"2")
+console.log("1"+2+2)
+console.log(1+2+"3")
+*/
+
