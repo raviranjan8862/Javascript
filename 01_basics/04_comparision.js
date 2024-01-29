f1047da41068db421b73c86503822d50112a0def
raviranjan8862 <121994910+raviranjan8862@users.noreply.github.com>
2024-01-29 13:24:08 +0000

    change in location in file

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,14 @@
+
+//console.log(2>1);
+//console.log(2>=1);
+//console.log(2<1);
+//console.log(2<=1);
+//console.log(2==1);
+//console.log(2!=1);
+
+
+//console.log("2"> 1)
+//console.log("02"> 1)
+
+
+// avoid compare between two datatype always compare in same datatypes
---
+++ 01_basics/Datatype-summary.js
@@ -0,0 +1,47 @@
+
+// #Types of datatypes in javascripts
+
+//# on the basic of storing and retreiving the data in memory or from memory then there are two datatype
+
+//#1. Primitive datatype
+
+/*
+7 Types of primitive datatypes:
+
+1.string 
+2.Number
+3.Boolean
+4.null
+5.udefined
+6.symbol
+7.BigInt
+*/
+ 
+//# it is dynamic type of programming language
+
+//#2.non-primitive datatype // refrence 
+/*
+1.Array
+2.objects
+3.function
+
+
+*/
+//array
+const heros=["shaktiman", "naagraj","doga"];
+
+//object
+let myobj={
+    name:"ravi",
+    age:22,
+
+}
+
+//function
+
+const myFunction=function()
+{
+    console.log("Hello world")
+}
+
+myFunction();
